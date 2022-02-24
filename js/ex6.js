7eea452e1a5480ae00594075db8e7dd136526fb6
mmcnicol40 <94950853+mmcnicol40@users.noreply.github.com>
2022-02-24 15:43:54 -0800

    Add files via upload

--- js/ex6.js
+++ js/ex6.js
@@ -2,10 +2,10 @@ console.log(" HW 4 ex 6 - Guess the Number");
 
 let lowNum = prompt("Please enter your lower range #:");
 let highNum = prompt("Please enter your upper range #"); // input's type is string
-let countNum = prompt("How many random #'s to generate?");
+//let countNum = prompt("How many random #'s to generate?");
 let nlowNum = Number(lowNum);
 let nhighNum = Number(highNum);
-let ncountNum = Number(countNum);
+let ncountNum = 1;
 let numAttempts = 0;
 let rndnum = 0;
 
@@ -20,18 +20,22 @@ for (numAttempts = 0; numAttempts < ncountNum; numAttempts++) {
 }
 console.log("test");
 console.log(`We generated ${ncountNum} random numbers for you.`);
+console.log("random # ", rndnum);
 let myguess = " ";
 let nmyGuess = 0;
-console.log("random # ", rndnum);
-let myGuess = prompt("Guess the random # I generated");
-nmyGuess = Number(myGuess);
-console.log(`guess ${nmyGuess}    rndnum = ${rndnum}`);
-console.log("guess matches", rndnum === nmyGuess);
+let numGuesses = 0;
 
-let newWord = "";
-let myCnt = 0;
-
-//while ((rndnum !== nmyGuess) || (myGuess !== "STOP")) {
-// myGuess = prompt("Guess the random # I generated or STOP to exit");
- // if (newWord !== "STOP") {
-  //   console.log("STOP TYPED");
+while (myguess != rndnum) {
+  myguess = prompt("Guess the random # I generated");
+  nmyGuess = Number(myguess);
+  if (nmyGuess > rndnum) {
+    console.log("your guess is too high")
+  }
+  if (nmyGuess < rndnum) {
+    console.log("your guess is too low")
+  }
+numGuesses = numGuesses + 1;
+}
+//console.log(`guess ${nmyGuess}    rndnum = ${rndnum}`);
+//console.log("guess matches", rndnum === nmyGuess);
+console.log(`Correct!  It took you ${numGuesses} guesses.`)
